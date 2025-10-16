a1ec362664825e17f09144981f2b49fb51ac869a
Alan Marques <eng.alanmarques@gmail.com>
2025-10-15 23:52:36 -0300

    capturar valor inicial do carrinho

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
-let soma = 0;
-
 function adicionar (){
 	let produtoEscolhido = document.getElementById('produto').value;
 	let quantidadeProduto = document.getElementById('quantidade').value;
 	let valor = parseFloat(produtoEscolhido.match(/\d/g).join(''));
+	somaValorDeProdutosAdicionados(valor);
 	
 	
 }
 
 function limpar (){
 	let
+}
+
+function somaValorDeProdutosAdicionados(valor){
+	let produtoInicial = document.getElementById('valor-total');
+	let valorInicial = parseFloat(produtoInicial.textContent.match(/\d/g).join(''));
+	
+	
 }
