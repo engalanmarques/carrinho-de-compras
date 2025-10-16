03bb6173dbb045c08dfc8cc69f86e7d0a2e19143
Alan Marques <eng.alanmarques@gmail.com>
2025-10-16 00:07:48 -0300

    atualizar o valor total do carrinho

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+let soma = 0;
 function adicionar (){
 	let produtoEscolhido = document.getElementById('produto').value;
 	let quantidadeProduto = document.getElementById('quantidade').value;
+	let quantidade = parseInt(quantidadeProduto);
 	let valor = parseFloat(produtoEscolhido.match(/\d/g).join(''));
-	somaValorDeProdutosAdicionados(valor);
+	soma = somaValorDeProdutosAdicionados(valor * quantidade);
+	alteraListaDeProdutos(produtoEscolhido, quantidadeProduto, soma);
+	console.log(soma);
 	
 	
 }
@@ -14,6 +18,13 @@ function limpar (){
 function somaValorDeProdutosAdicionados(valor){
 	let produtoInicial = document.getElementById('valor-total');
 	let valorInicial = parseFloat(produtoInicial.textContent.match(/\d/g).join(''));
+	let novaSoma = valorInicial + valor;
+	return novaSoma;
 	
-	
+}
+function alteraListaDeProdutos(produto, quantidade, soma){
+	let valorTotal = document.getElementById('valor-total');
+	console.log(soma);
+	valorTotal.textContent = `R$${soma}`;
+	//let novoProduto = document.getElementById();
 }
