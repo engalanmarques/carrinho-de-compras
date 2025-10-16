265e8b33c608cf3cfc28d313aed9e7a8a6dd82da
Alan Marques <eng.alanmarques@gmail.com>
2025-10-15 23:41:31 -0300

    capturar produto, quantidade e valor

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let soma = 0;
+
+function adicionar (){
+	let produtoEscolhido = document.getElementById('produto').value;
+	let quantidadeProduto = document.getElementById('quantidade').value;
+	let valor = parseFloat(produtoEscolhido.match(/\d/g).join(''));
+	
+	
+}
+
+function limpar (){
+	let
+}
